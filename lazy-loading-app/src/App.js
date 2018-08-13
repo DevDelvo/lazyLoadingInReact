2475151e6e04a566e9307b4e944b68e106acb2a9
DevDelvo <kevinbdelvo@gmail.com>
2018-08-13 16:48:10 -0400

    lazy loaded about

---
+++ lazy-loading-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import './App.css';
+import Home from './Home/Home.js';
+import About from './About/About.js';
+import Settings from './Settings/Settings.js';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <NavLink exact={true} activeClassName="active" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact={true} to="/about">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact={true} to="/settings">
+                Settings
+              </NavLink>
+            </li>
+          </ul>
+          <Route exact path="/" render={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/settings" Component={Settings} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
