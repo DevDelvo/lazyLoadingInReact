b01e11bf6fe004fc7867340e6930b128059fb667
DevDelvo <kevinbdelvo@gmail.com>
2018-08-13 16:56:38 -0400

    added edit profile lazy load

---
+++ lazy-loading-app/src/Settings/Settings.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import LazyEditProfile from './EditProfile';
+
+class Settings extends Component {
+    state = {
+        editable: false
+    };
+
+    editProfile = () => {
+        this.setState({ editable: true });
+    };
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button onClick={this.editProfile}>Edit Profile</Button>
+                    {this.state.editable && <LazyEditProfile />}
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Settings;
